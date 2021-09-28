0cf148ec2bd987aa68b7641adb7fe0a69ed5c221
Clem-Ferreol <83604723+Clem-Ferreol@users.noreply.github.com>
2021-09-28 21:29:29 +0200

    Fix pom & integration test

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,13 +1,14 @@
 package com.parkit.parkingsystem.integration;
-
-import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,8 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -28,6 +29,7 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
+    private String REGISTRATION_NUMBER = "ABCDEF";
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -46,9 +48,8 @@ public class ParkingDataBaseIT {
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         dataBasePrepareService.clearDataBaseEntries();
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn(REGISTRATION_NUMBER);
     }
 
     @AfterAll
@@ -58,28 +59,39 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+    	
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
        
-        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        Ticket ticket = ticketDAO.getTicket(REGISTRATION_NUMBER);
     	assertNotEquals(ticket, null);
-    	assertEquals(ticket.getVehicleRegNumber(), "ABCDEF");
+    	assertEquals(ticket.getVehicleRegNumber(), REGISTRATION_NUMBER);
     	assertEquals(ticket.getParkingSpot().isAvailable(), false);
         
     }
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
+        
+        Date date = DateUtils.addMinutes(new Date(), -15);
+        Ticket ticket = new Ticket();
+        ticket.setParkingSpot(new ParkingSpot(1,ParkingType.CAR, true));
+        ticket.setVehicleRegNumber(REGISTRATION_NUMBER);
+        ticket.setPrice(0);
+        ticket.setInTime(date);
+        ticket.setOutTime(null);
+        ticketDAO.saveTicket(ticket); 
+        
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         
-    	Ticket ticket = ticketDAO.getTicket("ABCDEF");
-    	assertNotEquals(ticket, null);
+    	Ticket outTicket = ticketDAO.getTicket(REGISTRATION_NUMBER);
+    	assertNotEquals(outTicket, null);
     	//Price should be free because the execution of the test is less than 30 minutes
-    	assertEquals(ticket.getPrice(), 0);
+    	assertEquals(outTicket.getPrice(), 0);
     	//Check if out time is null
-    	assertNotNull(ticketDAO.getTicket("ABCDEF").getOutTime());
+    	assertNotNull(outTicket.getOutTime());
     }
-
 }
